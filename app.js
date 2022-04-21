7edbcf377a63e6cb39b50e366a92b6886d4f76cb
Ludovic <ludovicgaron54@gmail.com>
2022-04-21 09:11:28 -0400

    cleanup + prettyTitle returned

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const request = require('request-promise');
+const request = require('request');
+const rp = require('request-promise');
 const cheerio = require('cheerio');
 const express = require('express');
 const http = require('http');
@@ -9,21 +10,21 @@ const fs = require('fs');
 
 var app = express();
 var server = http.createServer(app);
+var bookList;
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', async function (req, res) {
-  const bookList = await loadBookList();
-  res.render(path.join(__dirname, './book-list.html'), { bookList });
+  bookList = await loadBookList();
+  res.render(path.join(__dirname, './book-list.html'), { bookList: bookList.htmlBooks });
 });
 
 app.get('/bookIndexer', async function (req, res) {
   const bookPreview = req.query.book;
-  // console.log(bookPreview);
+  const prettyTitle = req.query.prettyTitle;
   const link = await getBookIndexPage(bookPreview);
-  //console.log(req.query.book.slice(0, req.query.book.indexOf("/")+1) + link);
-  res.redirect('/download?book=' + req.query.book.slice(0, req.query.book.lastIndexOf("/") + 1) + link);
+  res.redirect('/download?book=' + req.query.book.slice(0, req.query.book.lastIndexOf("/") + 1) + link + '&prettyTitle=' + prettyTitle);
 });
 
 app.get('/download', function (req, res) {
@@ -36,7 +37,7 @@ app.get('/download', function (req, res) {
       audioLinks.forEach((val, i, arr) => {
         bookList += "<li>" + val + "</li>";
       });
-      res.render(path.join(__dirname, './download.html'), { bookList });
+      res.render(path.join(__dirname, './download.html'), { bookList, prettyTitle: req.query.prettyTitle });
       // res.render(path.join(__dirname, './book-list.html'), { bookList });
       // res.send(audioLinks + "downloaded!");
       // const time = await new Promise(r => setTimeout(r, 2000));
@@ -52,7 +53,7 @@ server.listen(3000, function () {
 });
 
 async function loadBookList() {
-  let bookList = "";
+  let htmlBooks = "";
   const books = [];
 
   await fillBookList("https://esl-bits.net/ESL.English.Learning.Audiobooks/ESL.Classic.Audiobooks.htm", books);
@@ -60,35 +61,29 @@ async function loadBookList() {
 
   books.sort();
   books.forEach((val, i, arr) => {
-    bookList += val;
+    htmlBooks += val.listItem;
   });
-  return bookList;
+  return {
+    htmlBooks,
+    prettyTitles: books.map(a => a.prettifiedTitle),
+  };
 }
 
 async function fillBookList(url, books) {
-  const resp = await request(url, (error, response, html) => {
+  const resp = await rp(url, (error, response, html) => {
     const $ = cheerio.load(html);
     const output = $("a");
     output.each((i, element) => {
-      //https://esl-bits.net/ESL.English.Learning.Audiobooks/A.Christmas.Carol/preview.html
-      //https://esl-bits.net/ESL.English.Learning.Audiobooks/
-      const href = element.attribs["href"];
+      const href = element.attribs.href;
       if (href.includes("preview.html")) {
         let prettifiedTitle = element.children[0].data;
         let fallbackTitle = href.replace(/\./g, ' ').slice(0, href.lastIndexOf("/"));
         if (!prettifiedTitle) {
           prettifiedTitle = fallbackTitle;
         }
-        // else if (fallbackTitle.includes("/")) {
-        //   prettifiedTitle = fallbackTitle + "   " + prettifiedTitle;
-        // }
-
-        // if (prettifiedTitle.includes("Book")) {
-        //   // console.log(element);
-        // }
-        const listItem = "<li><a href=\"bookIndexer?book=https://esl-bits.net/ESL.English.Learning.Audiobooks/" + href + "\">" + prettifiedTitle + "</a></li>";
-        if (!books.some(book => book.includes(href))) {
-          books.push(listItem);
+        const listItem = "<li><a href=\"bookIndexer?book=https://esl-bits.net/ESL.English.Learning.Audiobooks/" + href + "&prettyTitle=" + prettifiedTitle + "\">" + prettifiedTitle + "</a></li>";
+        if (!books.some(book => book.listItem.includes(href))) {
+          books.push({ listItem, prettifiedTitle });
         }
       }
     });
@@ -97,13 +92,13 @@ async function fillBookList(url, books) {
 
 async function getBookIndexPage(previewPage) {
   let link;
-  const resp = await request(previewPage, (error, response, html) => {
+  const resp = await rp(previewPage, (error, response, html) => {
     if (!error && response.statusCode == 200) {
       const $ = cheerio.load(html);
       const output = $("a");
       output.each((i, element) => {
-        if (!element.attribs["href"].includes("/")) {
-          link = element.attribs["href"];
+        if (!element.attribs.href.includes("/")) {
+          link = element.attribs.href;
         }
       });
     }
@@ -113,25 +108,17 @@ async function getBookIndexPage(previewPage) {
 
 async function getAllChaptersLinks(queryInput) {
   const links = [];
-  const resp = await request(queryInput, (error, response, html) => {
+  const resp = await rp(queryInput, (error, response, html) => {
     if (!error && response.statusCode == 200) {
-      // console.log(queryInput);
-      const linkStart = queryInput.slice(0, queryInput.lastIndexOf("/") + 1);//.replace("indice.html", "");
+      const linkStart = queryInput.slice(0, queryInput.lastIndexOf("/") + 1);
       const $ = cheerio.load(html);
       const output = $("a");
       output.each((i, element) => {
-        //console.log(element.attribs["href"]);
-        const href = element.attribs["href"];
+        const href = element.attribs.href;
         if (!href.includes("http") && !href.includes("copyright")) {
           links.push(linkStart + href);
         }
       });
-      // const parts = $(".parts");
-      // const output = parts.find("a");
-      // output.each((i, element) => {
-      //   //console.log(linkStart + element.attribs["href"]);
-      //   links.push(linkStart + element.attribs["href"]);
-      // });
     }
   });
   return links;
@@ -141,15 +128,13 @@ async function getDownloadLinks(chaptersUrl) {
   const downloadLinks = [];
   for (let i = 0; i < chaptersUrl.length; i++) {
     const chapterUrl = chaptersUrl[i].slice(0, chaptersUrl[i].lastIndexOf('/') + 1);
-    // console.log(chapterUrl);
     const topUrl = chapterUrl + "top.html";
-    const resp = await request(topUrl, (error, response, html) => {
+    const resp = await rp(topUrl, (error, response, html) => {
       if (!error && response.statusCode == 200) {
         if (html.includes(".mp3")) {
-          let htmlBody = html.slice(html.indexOf("body"))
+          let htmlBody = html.slice(html.indexOf("body"));
           let stopAtMp3Point = htmlBody.slice(0, htmlBody.indexOf(".mp3"));
           const nameOfFile = stopAtMp3Point.slice(stopAtMp3Point.lastIndexOf('"') + 1) + ".mp3";
-          // console.log(nameOfFile);
           downloadLinks.push(chapterUrl + nameOfFile);
         }
       }
@@ -168,6 +153,7 @@ async function downloadLinks(audioUrls) {
         console.log("couldn't create /audiobooks folder");
         success = false;
       }
+      return true;
     });
     if (!success) {
       console.log("couldn't create /audiobooks folder");
@@ -178,7 +164,6 @@ async function downloadLinks(audioUrls) {
   let audio = audioUrls[0].slice(0, audioUrls[0].lastIndexOf("/"));
   const levels = audio.split('/');
   folder += levels[levels.length - 2];
-  // console.log(folder);
 
   if (fs.existsSync(folder)) {
     await safelyCreateFile(folder, audioUrls);
@@ -197,13 +182,11 @@ async function downloadLinks(audioUrls) {
 async function safelyCreateFile(folder, audioUrls) {
   for (let i = 0; i < audioUrls.length; i++) {
     const element = audioUrls[i];
-    // console.log(element);
     const levels = element.split('/');
     const filePath = folder + '/' + levels[levels.length - 2] + '.mp3';
-    // console.log(filePath);
     if (!fs.existsSync(filePath)) {
       let file = fs.createWriteStream(filePath);
-      const resp = await request(element, (error, response, html) => {
+      const resp = await rp(element, (error, response, html) => {
         if (!error && response.statusCode == 200) {
           console.log('creating file' + filePath);
         }
